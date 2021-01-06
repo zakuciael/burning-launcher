f11d7cbcec70e95f140f84d71e2538b415a28f90
Krzysztof Saczuk <zakuciael@outlook.com>
2021-01-06 19:10:36 +0100

    Improved Launcher class communication with external systems and fixed few bugs.

--- src/lib/launcher.ts
+++ src/lib/launcher.ts
@@ -1,6 +1,5 @@
 import { ChildProcess, spawn } from "child_process";
 import { EventEmitter } from "events";
-import isAdmin from "is-admin";
 import WebSocket from "ws";
 import path from "path";
 
@@ -19,56 +18,68 @@ export class Launcher extends EventEmitter {
         this.username = username;
     }
 
-    public async start(launcherPath: string): Promise<void> {
-        this.emit("update", `Spawning launcher process`);
-
-        if (!(await isAdmin())) {
-            this.emit("error", new Error("You need to run the program as the administrator"));
-            return;
-        }
-
+    public start(launcherPath: string): void {
         this.proc = spawn(path.resolve(launcherPath), [
             `burningsw://${this.token}/?username=${this.username}`,
         ]);
 
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        this.proc.on("error", () => {});
+
+        this.proc.on("close", (code) => {
+            this.emit("launcher_closed", code);
+
+            if (this.ws.readyState > WebSocket.CONNECTING) this.ws.close();
+            if (code !== 0) this.emit("error", new Error(`Process exited with error code ${code}`));
+            else if (code === 0 && !this.started) this.start(launcherPath);
+        });
+
         this.ws = new WebSocket(`wss://launcher.burningsw.to/ws`);
 
         this.ws.on("open", () => {
-            this.emit("update", `Authenticating to the launcher API`);
+            this.emit("update", `Connecting to the launcher...`);
             this.ws.send(this.token);
         });
 
         this.ws.on("message", (evt) => {
             const data = JSON.parse(evt as string);
-            this.ws.send("ping");
+
+            if (this.ws.readyState > WebSocket.CONNECTING && this.ws.readyState < WebSocket.CLOSING)
+                this.ws.send("ping");
 
             switch (data.type) {
                 case 0:
+                    this.emit("launcher_start");
                     this.emit("update", `Could not connect to the message broker server`);
                     break;
                 case 1:
-                    this.emit("update", `Connected to the launcher`);
+                    this.emit("launcher_start");
+                    this.emit("update", `Connection established successfully!`);
                     break;
                 case 2:
-                    this.emit("update", data.msg);
-                    this.emit("progress", {
-                        speed: data.speed,
-                        progress: data.progress,
-                    });
+                    if (
+                        !data.text ||
+                        data.text.includes("Launcher connected") ||
+                        data.text.includes("Starting the game client")
+                    )
+                        return;
+
+                    if (data.speed) {
+                        this.emit("file_download", {
+                            text: data.text,
+                            speed: data.speed,
+                            progress: data.progress || 0,
+                        });
+                    } else {
+                        this.emit("update", data.text);
+                    }
                     break;
                 case 3:
-                    this.emit("update", `Launcher started`);
+                    this.emit("update", `Game started`);
                     this.emit("game_started");
                     this.started = true;
                     break;
             }
-        });
-
-        this.proc.on("close", (code) => {
-            this.ws.close();
-
-            if (code !== 0) this.emit("error", new Error(`Process exited with error code ${code}`));
-            else if (code === 0 && !this.started) this.start(launcherPath);
         });
     }
 }
